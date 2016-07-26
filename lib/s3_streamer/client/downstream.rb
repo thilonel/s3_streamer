cb31c17c737a8afc88cecd4c9adad37ab0dc13ec
Tamas Drahos <drahostamas@gmail.com>
2016-07-26 10:16:32 +0200

    feat: use buffered writer instead of chunk buffer

--- lib/s3_streamer/client/downstream.rb
+++ lib/s3_streamer/client/downstream.rb
@@ -2,17 +2,30 @@ require 'net/http'
 require_relative 'downstream/chunk_buffer'
 
 class Downstream
-  def initialize(uri)
+  DEFAULT_PART_SIZE = 10 * 1024 * 1024
+
+  def initialize(uri, options={})
     @uri = uri
+    @options = options
   end
 
   def each
     Net::HTTP.start(@uri.host) do |http|
       http.request(Net::HTTP::Get.new @uri) do |response|
-        ChunkBuffer.new(response).each do |chunk|
-          yield chunk
+        buffer = BufferedWriter.new(min_part_size: part_size) do |part|
+          yield part
         end
+
+        response.read_body { |chunk| buffer.push chunk }
+
+        buffer.finish
       end
     end
+  end
+
+  private
+
+  def part_size
+    @options[:part_size] || DEFAULT_PART_SIZE
   end
 end
